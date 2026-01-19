b0fdf36ad8604881766175de1c6ef33f9b4d4116
BoyVOL <vladislav_limonov@mail.ru>
2026-01-19 10:41:48 +0900

    Added input handling

--- TestScenes/PlayerCharacter.cs
+++ TestScenes/PlayerCharacter.cs
@@ -8,4 +8,26 @@ public partial class PlayerCharacter : Node2D
     [Export]public String EventMoveDowm = "MoveDown";
     [Export]public String EventMoveLeft = "MoveLeft";
     [Export]public String EventMoveRight = "MoveRight";
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (@event.IsActionPressed(EventMoveUp))
+        {
+            GD.Print("Up");
+        }
+        if (@event.IsActionPressed(EventMoveDowm))
+        {
+            GD.Print("Down");
+        }
+        if (@event.IsActionPressed(EventMoveLeft))
+        {
+            GD.Print("Left");
+        }
+        if (@event.IsActionPressed(EventMoveRight))
+        {
+            GD.Print("Right");
+        }
+    }
+
 }
