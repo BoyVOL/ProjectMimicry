0c7f80dc5e5e2cac0cb70ece8c243404288bc13e
BoyVOL <vladislav_limonov@mail.ru>
2026-01-21 17:51:17 +0900

    Added multistate Sprite 2D and implemented in scene

---
+++ addons/multistate_sprite_module/MultistateSprite2D/MultistateSprite2D.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+[Tool]
+[GlobalClass]
+public partial class MultistateSprite2D : Sprite2D
+{
+}
--- addons/multistate_sprite_module/multistate_sprite_module.cs
+++ addons/multistate_sprite_module/multistate_sprite_module.cs
@@ -11,12 +11,15 @@ public partial class multistate_sprite_module : EditorPlugin
 	{
         GlobalPath = ((Resource)GetScript()).ResourcePath.GetBaseDir();
 		// Initialization of the plugin goes here.
+        AddCustomType("MultistateSprite2D", "Sprite2D", GD.Load<Script>(GlobalPath + "/MultistateSprite2D/MultistateSprite2D.cs"),
+        GD.Load<Texture2D>(GlobalPath + "/MultistateSprite2D/icon.png"));
 		GD.Print("AutoscalingViewportModule loaded");
 	}
 
 	public override void _ExitTree()
 	{
 		// Clean-up of the plugin goes here.
+		RemoveCustomType("MultistateSprite2D");
 		GD.Print("AutoscalingViewportModule unloaded");
 	}
 }
