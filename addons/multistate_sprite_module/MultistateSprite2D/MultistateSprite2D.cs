750ac05f29298dd190555dbb7fb969c16d8bd932
BoyVOL <vladislav_limonov@mail.ru>
2026-01-22 11:16:13 +0900

    State change implemented

--- addons/multistate_sprite_module/MultistateSprite2D/MultistateSprite2D.cs
+++ addons/multistate_sprite_module/MultistateSprite2D/MultistateSprite2D.cs
@@ -6,4 +6,19 @@ using System;
 public partial class MultistateSprite2D : Sprite2D
 {
     [Export]Texture2D[] States = new Texture2D[4];
+
+    [Export]int CurrentState = 0;
+
+    [ExportToolButton("TestState")]
+    public Callable TestChange => Callable.From(ToCurrentState);
+
+    public void ChangeState(int StateId)
+    {
+        Texture = States[StateId];
+    }
+
+    public void ToCurrentState()
+    {
+        ChangeState(CurrentState);
+    }
 }
