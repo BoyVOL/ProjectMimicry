7288167ac55ad6289b2aaa9cd3e72a8732968f50
BoyVOL <vladislav_limonov@mail.ru>
2026-04-04 18:21:39 +0900

    Implemented navigation agents

--- Gridmap Test 2/Scenes/PathfindTest.cs
+++ Gridmap Test 2/Scenes/PathfindTest.cs
@@ -4,4 +4,18 @@ using System;
 public partial class PathfindTest : RigidBody3D
 {
     [Export] Node3D PathfindTarget = null;
+    [Export] NavigationAgent3D Navigator = null;
+    Vector3 NextPathPoint = Vector3.Zero;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if(PathfindTarget != null && Navigator != null)
+        {
+            Navigator.TargetPosition = PathfindTarget.GlobalPosition;
+            NextPathPoint = Navigator.GetNextPathPosition();
+            GD.Print(NextPathPoint);
+        }
+        base._PhysicsProcess(delta);
+    }
+
 }
