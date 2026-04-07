738c912c8382429851c2a488369280860bd92960
BoyVOL <vladislav_limonov@mail.ru>
2026-04-08 02:39:07 +0900

    Midpoint save

--- Gridmap Test 2/Scenes/TestPlayer.cs
+++ Gridmap Test 2/Scenes/TestPlayer.cs
@@ -3,44 +3,8 @@ using System;
 
 public partial class TestPlayer : RigidBody3D
 {
-	[Export]public String EventMoveUp = "MoveUp";    
-	
-	[Export]public String EventMoveDowm = "MoveDown";
-	[Export]public String EventMoveLeft = "MoveLeft";
-	[Export]public String EventMoveRight = "MoveRight";
-
-	[Export]float MovementSpeed = 10;
-
-	[Export] RigidBodyPid PIDNode = null;
-
-	V3PID MovePID = new V3PID();
-
-	public Vector3 GetMovementVector()
-	{
-		Vector3 Result = Vector3.Zero;
-		
-		if (Input.IsActionPressed(EventMoveLeft)){
-			Result.X -= 1;
-		}
-		if (Input.IsActionPressed(EventMoveRight)){
-			Result.X += 1;
-		}
-		if (Input.IsActionPressed(EventMoveUp)){
-			Result.Z -= 1;
-		}
-		if (Input.IsActionPressed(EventMoveDowm))
-		{
-			Result.Z += 1;
-		}
-		return Result;
-	}
-
 	public override void _PhysicsProcess(double delta)
 	{
-		if(PIDNode != null)
-		{
-			PIDNode.DesiredVector = MovementSpeed*GetMovementVector();
-		}
 		base._PhysicsProcess(delta);
 	}
 
