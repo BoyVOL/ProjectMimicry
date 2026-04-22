f0daa1644895524457826ec511272c1d4c45005b
BoyVOL <vladislav_limonov@mail.ru>
2026-04-22 16:42:10 +0900

    Created Debug Node

--- addons/statechartsexpancion/statechartsexpancion.cs
+++ addons/statechartsexpancion/statechartsexpancion.cs
@@ -4,15 +4,23 @@ using System;
 
 [Tool]
 public partial class statechartsexpancion : EditorPlugin
-{
+{ 
+	String GlobalPath;
+
 	public override void _EnterTree()
 	{
+        GlobalPath = ((Resource)GetScript()).ResourcePath.GetBaseDir();
 		// Initialization of the plugin goes here.
+        AddCustomType("StateDebug", "Node", GD.Load<Script>(GlobalPath + "/StateDebug/StateDebug.cs"),
+        GD.Load<Texture2D>(GlobalPath + "/StateDebug/icon.png"));
+		GD.Print("statechartsexpancion loaded");
 	}
 
 	public override void _ExitTree()
 	{
 		// Clean-up of the plugin goes here.
+		RemoveCustomType("StateDebug");
+		GD.Print("statechartsexpancion unloaded");
 	}
 }
 #endif
