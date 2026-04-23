d28ab34061dfa57f5c20b0165d150745b4068a5f
BoyVOL <vladislav_limonov@mail.ru>
2026-04-23 12:35:03 +0900

    Testing statechart expressions

--- addons/statechartsexpancion/StateDebug/StateDebug.cs
+++ addons/statechartsexpancion/StateDebug/StateDebug.cs
@@ -1,6 +1,25 @@
 using Godot;
+using GodotStateCharts;
 using System;
 
 public partial class StateDebug : Node
 {
+    [Export] Node State = null;
+
+    StateChartState WrappedState = null;
+
+    public override void _EnterTree()
+    {
+        if (State != null)
+        {
+            WrappedState = StateChartState.Of(State);
+        }
+        base._EnterTree();
+    }
+
+    public override void _Process(double delta)
+    {
+        GD.Print(WrappedState.Active);
+        base._Process(delta);
+    }
 }
